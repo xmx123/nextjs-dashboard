1b9230220c5330e01919766b966c8bc21446bff5
xmx <mingxin.xu@freebrio.com>
2024-11-05 15:30:15 +0800

    NavLinks

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import '@/app/ui/global.css';
+import { inter } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='zh-CN'>
+      <body className={`${inter.className} antaliased`}>{children}</body>
     </html>
   );
 }
